d685e8586a445e01ad3b16ce91748f2cfa031a14
Wstld <pontus.westlund@gmail.com>
2020-12-22 17:50:02 +0100

    Score labels added. View is also updated to dynamically fit any screen.

--- UIkitBuild/Controller/GameBoardViewController.swift
+++ UIkitBuild/Controller/GameBoardViewController.swift
@@ -11,8 +11,15 @@ class GameBoardViewController: UIViewController{
     var game = GameModel()
     var currentPlayerLable:UILabel!
     var gameBoard:GameBoard!
-    var textLable:UILabel!
-    var gameOver = false
+    
+    var statsHeadline:UILabel!
+    
+    var player1NameLable:UILabel!
+    var player2NameLable:UILabel!
+    
+    var player1ScoreLable:UILabel!
+    var player2ScoreLable:UILabel!
+    
     override func loadView() {
         
         //Main Canvas.
@@ -41,6 +48,33 @@ class GameBoardViewController: UIViewController{
         currentPlayerLable.text = game.currentPlayer
         currentPlayerLable.font = UIFont.systemFont(ofSize: 40.0)
         currentPlayerLable.translatesAutoresizingMaskIntoConstraints = false
+        
+        statsHeadline = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth/3, height: screenWidth/6))
+        statsHeadline.translatesAutoresizingMaskIntoConstraints = false
+        statsHeadline.font = UIFont.systemFont(ofSize: 40)
+        statsHeadline.text = "Score"
+        
+        player1NameLable = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth/3, height: screenWidth/6))
+        player1NameLable.translatesAutoresizingMaskIntoConstraints = false
+        player1NameLable.font = UIFont.systemFont(ofSize: 30)
+        player1NameLable.text = self.game.Player1 + ":"
+        
+        player2NameLable = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth/3, height: screenWidth/6))
+        player2NameLable.translatesAutoresizingMaskIntoConstraints = false
+        player2NameLable.font = UIFont.systemFont(ofSize: 30)
+        player2NameLable.text = self.game.Player2 + ":"
+        
+        player1ScoreLable = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth/3, height: screenWidth/6))
+        player1ScoreLable.font = UIFont.systemFont(ofSize: 30)
+        player1ScoreLable.translatesAutoresizingMaskIntoConstraints = false
+        player1ScoreLable.text = "10"
+        
+        player2ScoreLable = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth/3, height: screenWidth/6))
+        player2ScoreLable.font = UIFont.systemFont(ofSize: 30)
+        player2ScoreLable.translatesAutoresizingMaskIntoConstraints = false
+        player2ScoreLable.text = "10"
+        
+        
         
          //Set onClick action for gameboard
          gameBoard.clickAction = { (btn:UIButton) in
@@ -78,15 +112,39 @@ class GameBoardViewController: UIViewController{
          //Sub views added to main canvas.
          view.addSubview(gameBoard)
         view.addSubview(currentPlayerLable)
+        view.addSubview(player2NameLable)
+        view.addSubview(player1NameLable)
+        view.addSubview(statsHeadline)
+        view.addSubview(player2ScoreLable)
+        view.addSubview(player1ScoreLable)
          
          //Constraints for all subviews.
          NSLayoutConstraint.activate([
-             //gameboard
-             gameBoard.topAnchor.constraint(equalTo: view.topAnchor,constant: UIScreen.main.bounds.height * 0.2),
-             gameBoard.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             //current player lable
             currentPlayerLable.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor,constant: 10),
-            currentPlayerLable.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            currentPlayerLable.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            //gameboard
+            gameBoard.topAnchor.constraint(equalTo: currentPlayerLable.bottomAnchor,constant: 15),
+             gameBoard.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+           
+            //player2 name lable
+            player2NameLable.bottomAnchor.constraint(equalTo:view.layoutMarginsGuide.bottomAnchor,constant: screenWidth * -0.1),
+            player2NameLable.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor),
+            //player 2 score lable
+            player2ScoreLable.leftAnchor.constraint(equalTo: player2NameLable.rightAnchor,constant: 15),
+            player2ScoreLable.bottomAnchor.constraint(equalTo: player2NameLable.bottomAnchor),
+            
+            //player1 name lable
+            player1NameLable.bottomAnchor.constraint(equalTo:player2NameLable.topAnchor,constant: 10 ),
+            player1NameLable.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor),
+            //player 1 score lable
+            player1ScoreLable.leftAnchor.constraint(equalTo: player2ScoreLable.leftAnchor),
+            player1ScoreLable.bottomAnchor.constraint(equalTo: player1NameLable.bottomAnchor),
+            
+            //Stats Headline lable
+            statsHeadline.bottomAnchor.constraint(equalTo: player1NameLable.topAnchor,constant: 10),
+            statsHeadline.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor)
              
          ])
     }
